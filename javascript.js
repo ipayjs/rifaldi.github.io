db17af7f5e25860d3f2bebded49fa3d7adb684cc
ipayjs <muhamadrifaldi@smkwikrama.sch.id>
2023-11-30 19:03:40 +0700

    tambahan dikit

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let message = prompt("pendapat anda tentang website saya?");
+let message = prompt("bagaimana pendapat anda tentang website saya?");
 alert(
   "pendapat anda tentang website saya adalah " +
     message +
